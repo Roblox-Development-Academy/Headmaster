6843ea939707986b04812037875d928fb67515d6
Velinquish <88jliu88@gmail.com>
2020-06-29 04:16:05 -0400

    refactor: moved helpers.prompt() to common.prompt(); prepare to delete common.py

--- helpers.py
+++ helpers.py
@@ -1,10 +1,10 @@
-import asyncio
 from datetime import datetime
 from dateutil.parser import parse
 import pytz
 
 from bot import *
-import errors
+
+# TODO - Delete this module and replace it with common.py
 
 message = {
     'cancel_prompt': "\n\nRespond with `cancel` to cancel the setup.",
@@ -86,36 +86,6 @@ def usage_embed(usage, description, example=None):
         embed.add_field(name='Example', value=f"`{example}`")
 
     return embed
-
-
-async def prompt(channel: discord.TextChannel, user: discord.User, *args, prompt_msg: discord.Message = None,
-                 timeout=300, **kwargs) -> discord.Message:
-    """
-    Prompts the specified user for a text response
-
-    :param user: The specific user to prompt
-    :param channel: The channel to send the prompt in
-    :param prompt_msg: The message object to edit if error
-    :param timeout: How many seconds before timeout
-    :return: The user's response
-    """
-    # TODO - Add the user to a dictionary whose commands are not seen in the channel due to being in the prompt
-    if not prompt_msg:
-        prompt_msg = await channel.send(*args, **kwargs)
-
-    def check(m):
-        return m.author == user and m.channel == channel
-
-    msg = None
-    try:
-        msg = await client.wait_for("message", check=check, timeout=timeout)
-    except asyncio.TimeoutError:
-        raise errors.PromptTimeout("The prompt has timed out", prompt_msg)
-
-    if msg.content.lower() == "cancel":
-        raise errors.PromptCancelled("The prompt was cancelled", prompt_msg)
-
-    return msg
 
 
 def retrieve_assignments(user_id) -> tuple:
