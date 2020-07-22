3daef53dfba1e927cc2792eae00ee52f3653d5cb
1624252 <sylviapenguin@gmail.com>
2020-07-22 15:56:05 -0500

    feat(report): add report reaction command

--- cogs/report.py
+++ cogs/report.py
@@ -19,7 +19,7 @@ class Report(commands.Cog):
         if str(payload.emoji) == lang.global_placeholders.get("emoji.report"):
             report_channel = self.client.get_channel(self.report_channel)
             await message.remove_reaction(payload.emoji, payload.member)
-            await lang.get("report.start").send(report_channel, reported=f"{message.author}", reported_id=str(message.author.id), reporter=f"{payload.member}", reporter_id=str(payload.member.id), guild_name=message.guild.name, guild_id=str(message.guild.id), channel_name=message.channel.name, channel_id=str(message.channel.id), jump_url=message.jump_url, message_id=str(message.id), message_sent_at=str(message.created_at), report_time=str(datetime.now(timezone.utc)))
+            await lang.get("report.start").send(report_channel, reported=f"{message.author}", reported_id=str(message.author.id), reporter=f"{payload.member}", reporter_id=str(payload.member.id), guild_name=message.guild.name, guild_id=str(message.guild.id), channel_name=message.channel.name, channel_id=str(message.channel.id), jump_url=message.jump_url, message_id=str(message.id), message_sent_at=message.created_at.strftime('%A, %B %d, %Y; %I:%M %p UTC.'), report_time=datetime.now(timezone.utc).strftime('%A, %B %d, %Y; %I:%M %p UTC.'))
             message_copy = await MessageNode.from_message(message)
             await message_copy.send(report_channel)
             await lang.get("report.end").send(report_channel)
