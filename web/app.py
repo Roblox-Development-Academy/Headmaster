0b774465d89d740d4ea740ff44bbe791a0dfea16
Liu Jiawei <88jliu88@gmail.com>
2020-07-30 21:52:09 -0400

    feat: add webserver

--- web/app.py
+++ web/app.py
@@ -1,18 +1,42 @@
-from fastapi import FastAPI, Request, Query
+from datetime import datetime, timezone
+
+from fastapi import FastAPI, Request, Query, Body
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
+
+import events
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="web/static"), name="static")
+app.mount("/datepicker", StaticFiles(directory="node_modules/air-datepicker/dist"), name="datepicker")
+# app.mount("/html2canvas", StaticFiles(directory="node_modules/html2canvas/dist"), name="html2canvas")
 
-templates = Jinja2Templates(directory="webserver/templates")
+templates = Jinja2Templates(directory="web/templates")
+
+
+def render_template(path: str, request: Request, **kwargs):
+    return templates.TemplateResponse(path, {'request': request, **kwargs})
 
 
 @app.get('/')
 async def index():
-    return {'value:': 'hello world'}
+    return {'value:': 'Oh, hello! Welcome to our beautiful homepage :D'}
 
 
-@app.get('/datetime/')
-async def dynamic(request: Request, user_id: int = Query(..., alias="user-id")):
-    return templates.TemplateResponse("date_select.html", {
-        'request': request
-    })
+@app.get('/date-select/')
+async def select_datetime(request: Request, user_id: int = Query(..., alias="user-id")):
+    return render_template('date_select.html', request, user_id=user_id)
+
+
+class Date(BaseModel):
+    date: float
+
+
+@app.post('/date-select/')
+async def select_datetime_submit(user_id: int = Query(..., alias='user-id'), date: Date = Body(...)):
+    try:
+        result = datetime.fromtimestamp(date.date, timezone.utc)
+    except Exception:
+        result = None
+    await events.date_selected.fire(user_id, result)
