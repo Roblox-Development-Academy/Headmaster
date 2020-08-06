494dbbe9d029cf7872ebc75253c7f3ab4d2e9e4d
Liu Jiawei <88jliu88@gmail.com>
2020-08-06 17:38:59 -0400

    feat: add globals rda, class_channel, class_category, teacher_role

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os as __os
 import logging as __logging
+import asyncio as __asyncio
 
 import discord
 from discord.ext import commands
@@ -50,8 +51,32 @@ def get_mention_or_prefix(_, message):
 
 client = commands.Bot(command_prefix=get_mention_or_prefix, case_insensitive=True, help_command=None)
 
-rda: discord.Guild = client.get_guild(676175299121250327)
-class_channel: discord.TextChannel = rda.get_channel(739213440803012608)
+
+rda: discord.Guild
+class_channel: discord.TextChannel
+class_category: discord.CategoryChannel
+teacher_role: discord.Role
+
+__loop = __asyncio.get_event_loop()
+__loop.create_task(client.start(TOKEN))
+
+
+@client.listen('on_ready')
+async def __on_ready():
+    global rda, class_channel, class_category, teacher_role
+    if __os.environ['DEBUG']:
+        rda = client.get_guild(676175299121250327)
+        class_channel = rda.get_channel(739213440803012608)
+        class_category = rda.get_channel(677766311530594305)
+        teacher_role = rda.get_role(677766292714815508)
+        logger.info("Created globals using DEBUG set")
+    else:
+        rda = client.get_guild(673600024919408680)
+        class_channel = rda.get_channel(673604720601858069)
+        class_category = rda.get_channel(673604345316638730)
+        teacher_role = rda.get_role(673608309198028811)
+        logger.info("Created globals using production set")
+    logger.info(f"Logged in as {client.user}. I am in {len(client.guilds)} guilds.")
 
 in_prompt = {}  # Dict of user IDs to their prompt message URLs; users in the middle of a prompt can't run commands
 
--- main.py
+++ main.py
@@ -3,163 +3,149 @@ import re
 
 import uvicorn
 
-from bot import *
-from bot import in_prompt
-from cogs.admin import Admin
-from cogs.errorhandler import ErrorHandler
-import conditions
 from web.app import app
+from bot import *
 
 
-def generate_tables():
-    statements = (
-        f"""
-        CREATE TABLE IF NOT EXISTS guilds(
-            id BIGINT PRIMARY KEY UNIQUE NOT NULL,
-            prefix TEXT DEFAULT '{lang.global_placeholders.get('default_prefix')}'
+async def run():
+    await client.wait_until_ready()
+    await asyncio.sleep(1)
+    from cogs.admin import Admin
+    from cogs.errorhandler import ErrorHandler
+    import conditions
+
+    def generate_tables():
+        statements = (
+            f"""
+            CREATE TABLE IF NOT EXISTS guilds(
+                id BIGINT PRIMARY KEY UNIQUE NOT NULL,
+                prefix TEXT DEFAULT '{lang.global_placeholders.get('default_prefix')}'
+            )
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS ignored_channels(
+                id BIGINT PRIMARY KEY UNIQUE NOT NULL,
+                guild_id BIGINT NOT NULL
+            )
+            """,
+            """
+            CREATE INDEX IF NOT EXISTS idx_channel_guild ON ignored_channels(guild_id)
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS assignments(
+                assigner BIGINT NOT NULL,
+                name TEXT NOT NULL,
+                description BIGINT,
+                solution BIGINT,
+                delete_after_date BOOLEAN,
+                date TIMESTAMPTZ,
+                interval INTERVAL,
+                PRIMARY KEY (assigner, name)
+            )
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS submissions(
+                submitter BIGINT NOT NULL,
+                assigner BIGINT NOT NULL,
+                name TEXT NOT NULL,
+                submitted_at TIMESTAMPTZ NOT NULL,
+                PRIMARY KEY (submitter, assigner, name),
+                CONSTRAINT fk_assignment FOREIGN KEY(assigner, name) REFERENCES assignments(assigner, name)
+                ON DELETE CASCADE
+            )
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS classes(
+                teacher BIGINT NOT NULL,
+                name TEXT NOT NULL,
+                message BIGINT,
+                guild BIGINT,
+                channel BOOLEAN,
+                voice_channel BOOLEAN,
+                starting_at TIMESTAMPTZ NOT NULL,
+                PRIMARY KEY (teacher, name)
+            )
+            """
         )
-        """,
-        """
-        CREATE TABLE IF NOT EXISTS ignored_channels(
-            id BIGINT PRIMARY KEY UNIQUE NOT NULL,
-            guild_id BIGINT NOT NULL
-        )
-        """,
-        """
-        CREATE INDEX IF NOT EXISTS idx_channel_guild ON ignored_channels(guild_id)
-        """,
-        """
-        CREATE TABLE IF NOT EXISTS assignments(
-            assigner BIGINT NOT NULL,
-            name TEXT NOT NULL,
-            description BIGINT,
-            solution BIGINT,
-            delete_after_date BOOLEAN,
-            date TIMESTAMPTZ,
-            interval INTERVAL,
-            PRIMARY KEY (assigner, name)
-        )
-        """,
-        """
-        CREATE TABLE IF NOT EXISTS submissions(
-            submitter BIGINT NOT NULL,
-            assigner BIGINT NOT NULL,
-            name TEXT NOT NULL,
-            submitted_at TIMESTAMPTZ NOT NULL,
-            PRIMARY KEY (submitter, assigner, name),
-            CONSTRAINT fk_assignment FOREIGN KEY(assigner, name) REFERENCES assignments(assigner, name)
-            ON DELETE CASCADE
-        )
-        """,
-        """
-        CREATE TABLE IF NOT EXISTS classes(
-            teacher BIGINT NOT NULL,
-            name TEXT NOT NULL,
-            message BIGINT,
-            guild BIGINT,
-            channel BIGINT,
-            voice_channel BIGINT,
-            starting_at TIMESTAMPTZ NOT NULL,
-            PRIMARY KEY (teacher, name)
-        )
-        """
-    )
-    for statement in statements:
-        database.update(statement)
+        for statement in statements:
+            database.update(statement)
 
+    generate_tables()
 
-generate_tables()
+    client.add_cog(ErrorHandler())
+    client.add_cog(Admin())
+    client.load_extension('cmds.apply')
+    client.load_extension('cmds.homework')
+    client.load_extension('cmds.class')
 
+    @client.event
+    async def on_message(msg):
+        await client.process_commands(msg)
+        pass
 
-client.add_cog(ErrorHandler())
-client.add_cog(Admin())
-client.load_extension('cmds.apply')
-client.load_extension('cmds.homework')
-client.load_extension('cmds.class')
-
-
-@client.event
-async def on_ready():
-    logger.info(f"Logged in as {client.user}. I am in {len(client.guilds)} guilds.")
-
-
-@client.event
-async def on_message(msg):
-    await client.process_commands(msg)
-    pass
-
-
-@client.check
-async def globally_ignore_channels(ctx):
-    if database.query(
-        """
-        SELECT id
-        FROM ignored_channels
-        WHERE id = %s
-        """,
-        (ctx.channel.id,)
-    ).fetchone() is None:
-        return True
-    else:
-        await ctx.message.delete()
-        await lang.get('error.ignored_channel').send(ctx)
-        return False
-
-
-@client.check
-async def globally_ignore_banned(ctx):
-    # TODO
-    return True
-
-
-@client.check
-async def ignore_in_prompt(ctx):
-    prompt = in_prompt.get(ctx.message.author.id)
-    if prompt:
-        try:
+    @client.check
+    async def globally_ignore_channels(ctx):
+        if database.query(
+                """
+            SELECT id
+            FROM ignored_channels
+            WHERE id = %s
+            """,
+                (ctx.channel.id,)
+        ).fetchone() is None:
+            return True
+        else:
             await ctx.message.delete()
-        except discord.errors.Forbidden:
-            pass
-        await lang.get('error.in_prompt').send(ctx, prompt=prompt)
-        return False
-    return True
+            await lang.get('error.ignored_channel').send(ctx)
+            return False
 
+    @client.check
+    async def globally_ignore_banned(ctx):
+        # TODO
+        return True
 
-# help is a default command - could be overridden if it looks ugly
-@client.command(aliases=["help", "cmds", "commands"])
-async def _help(ctx):
-    await lang.get('help').send(ctx)
+    @client.check
+    async def ignore_in_prompt(ctx):
+        prompt = in_prompt.get(ctx.message.author.id)
+        if prompt:
+            try:
+                await ctx.message.delete()
+            except discord.errors.Forbidden:
+                pass
+            await lang.get('error.in_prompt').send(ctx, prompt=prompt)
+            return False
+        return True
 
+    # help is a default command - could be overridden if it looks ugly
+    @client.command(aliases=["help", "cmds", "commands"])
+    async def _help(ctx):
+        await lang.get('help').send(ctx)
 
-@client.command(name="reverse")
-async def reverse_poem(ctx, msg: discord.Message):
-    lines = msg.content.split("\n")
-    await ctx.send("\n".join(reversed(lines)))
+    @client.command(name="reverse")
+    async def reverse_poem(ctx, msg: discord.Message):
+        lines = msg.content.split("\n")
+        await ctx.send("\n".join(reversed(lines)))
 
+    @client.command(name='command')
+    async def command(ctx):
+        await lang.get('error.command').send(ctx, prefix=get_prefix(ctx.guild.id))
 
-@client.command(name='command')
-async def command(ctx):
-    await lang.get('error.command').send(ctx, prefix=get_prefix(ctx.guild.id))
+    @client.command(aliases=('exec',))
+    @conditions.manager_only()
+    async def execute(ctx: commands.Context):
+        content = ctx.message.content
+        matcher = re.compile(r'```\w+$(.+)```', re.MULTILINE | re.DOTALL)
+        code = matcher.search(content)
+        if code:
+            try:
+                exec(code.group(1), globals(), locals())
+            except Exception as e:
+                await ctx.send(str(e))
+            await ctx.message.add_reaction(lang.global_placeholders['emoji.gotcha'])
+        else:
+            await ctx.message.add_reaction(lang.global_placeholders['emoji.error'])
 
-
-@client.command(aliases=('exec',))
-@conditions.manager_only()
-async def execute(ctx: commands.Context):
-    content = ctx.message.content
-    matcher = re.compile(r'```\w+$(.+)```', re.MULTILINE | re.DOTALL)
-    code = matcher.search(content)
-    if code:
-        try:
-            exec(code.group(1), globals(), locals())
-        except Exception as e:
-            await ctx.send(str(e))
-        await ctx.message.add_reaction(lang.global_placeholders['emoji.gotcha'])
-    else:
-        await ctx.message.add_reaction(lang.global_placeholders['emoji.error'])
-
-
-loop = asyncio.get_event_loop()
-loop.create_task(client.start(TOKEN))
+asyncio.create_task(run())
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=5000)
