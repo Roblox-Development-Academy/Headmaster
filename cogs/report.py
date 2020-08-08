a1526a8bac438b48825dc88cf71acd1b93fd580b
1624252 <sylviapenguin@gmail.com>
2020-08-07 21:59:43 -0500

    fix(report): do not allow user to report bot

--- cogs/report.py
+++ cogs/report.py
@@ -12,15 +12,18 @@ class Report(commands.Cog):
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
-        message = await client.get_channel(payload.channel_id).fetch_message(payload.message_id)
         if str(payload.emoji) == lang.global_placeholders.get("emoji.report"):
-            report_channel = client.get_channel(self.report_channel)
+            message = await client.get_channel(payload.channel_id).fetch_message(payload.message_id)
             await message.remove_reaction(payload.emoji, payload.member)
+            if message.author == client.user:
+                await lang.get("error.report").send(message.channel)
+                return
+            report_channel = client.get_channel(self.report_channel)
             await lang.get("report.start").send(report_channel, reported=f"{message.author}", reported_id=str(message.author.id), reporter=f"{payload.member}", reporter_id=str(payload.member.id), guild_name=message.guild.name, guild_id=str(message.guild.id), channel_name=message.channel.name, channel_id=str(message.channel.id), jump_url=message.jump_url, message_id=str(message.id), message_sent_at=message.created_at.strftime('%A, %B %d, %Y; %I:%M %p UTC.'), report_time=datetime.now(timezone.utc).strftime('%A, %B %d, %Y; %I:%M %p UTC.'))
             message_copy = await MessageNode.from_message(message)
             try:
                 await message_copy.send(report_channel)
             except HTTPException:
-                await report_channel.send(embed=Embed(description="The reported message has no content.", colour=int(lang.global_placeholders.get("color.info"), 16)))
+                await report_channel.send(embed=Embed(description="The reported message has no content.", colour=int(lang.global_placeholders.get("color.error"), 16)))
             await lang.get("report.end").send(report_channel)
             await lang.get("report.success").send(payload.member, reported_id=str(message.author.id), reported_name=message.author.name)
