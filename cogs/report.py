ec8965207688e68b4b9b5be759e55dcb8d4a0230
1624252 <sylviapenguin@gmail.com>
2021-10-18 22:54:25 -0500

    refactor(report): implement new timestamps

--- cogs/report.py
+++ cogs/report.py
@@ -2,6 +2,7 @@ from datetime import timezone, datetime
 from nextcord.ext import commands
 from bot import lang, client, report_channel
 from utils.language import MessageNode
+from utils.common import to_unix
 from nextcord import Embed
 from nextcord.errors import HTTPException
 
@@ -15,15 +16,15 @@ class Report(commands.Cog):
             if message.author == client.user:
                 await lang.get("error.report").send(message.channel)
                 return
-            time_format = '%A, %B %d, %Y; %I:%M %p UTC.'
+            # time_format = '%A, %B %d, %Y; %I:%M %p UTC'
             await lang.get("report.start").send(report_channel, reported=f"{message.author}",
                                                 reported_id=str(message.author.id), reporter=f"{payload.member}",
                                                 reporter_id=str(payload.member.id), guild_name=message.guild.name,
                                                 guild_id=str(message.guild.id), channel_name=message.channel.name,
                                                 channel_id=str(message.channel.id), jump_url=message.jump_url,
                                                 message_id=str(message.id),
-                                                message_sent_at=message.created_at.strftime(time_format),
-                                                report_time=datetime.now(timezone.utc).strftime(time_format))
+                                                message_sent_at=to_unix(message.created_at),
+                                                report_time=to_unix(datetime.now(timezone.utc)))
             message_copy = await MessageNode.from_message(message)
             try:
                 await message_copy.send(report_channel)
